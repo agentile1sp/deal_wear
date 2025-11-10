da87976491311da833602ecf74992c2d80c23e3a
Andres Publio Gentile <apgs99@gmail.com>
2025-11-09 22:21:43 -0500

    chore: settings add Static

--- deal_wear_code/deal_wear_code/settings.py
+++ deal_wear_code/deal_wear_code/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = "deal_wear_code.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -68,6 +68,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "deal_wear_code.wsgi.application"
+
+#Static
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Database
